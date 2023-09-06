cd947221261aeb83f788ba7358fc3235c7ccf995
Anna Markina <rebane2118@gmail.com>
2023-09-06 16:28:40 +0300

    save client data from form

--- study/FoodMarket/Food/dist/js/script.js
+++ study/FoodMarket/Food/dist/js/script.js
@@ -213,4 +213,58 @@ window.addEventListener('DOMContentLoaded', () => {
         '.menu .container',
         'menu__item'
     ).render();
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с Вами свяжемся ;)',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMassage = document.createElement('div');
+            statusMassage.classList.add('status');
+            statusMassage.textContent = message.loading;
+            form.append(statusMassage);
+
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMassage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMassage.remove();
+                    }, 2000);
+                } else {
+                    statusMassage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
